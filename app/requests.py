495f0b913e3ebae4ca052bf32a49d8318b4cbff9
becky nayere <rebecca.nayere@gmail.com>
2020-09-30 11:52:09 +0300

    create a styles file

---
+++ app/requests.py
@@ -0,0 +1,7 @@
+import requests,json
+
+def get_quotes():
+    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if response.status_code == 200:
+        quote = response.json()
+        return quote
