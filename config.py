654afcfffda57346f0a5560c5d44017dd149e06a
becky nayere <rebecca.nayere@gmail.com>
2020-10-01 22:53:39 +0300

    add mail use ssl

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class Config:
     MAIL_SERVER ='smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
 
