53ae6cfcecf22b5b4a0b17ae6ebd11c527819cae
becky nayere <rebecca.nayere@gmail.com>
2020-10-07 16:10:30 +0300

    add a subscriber.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 
-app = create_app()
+app = create_app('development')
 
 manager =  Manager(app)
 migrate = Migrate(app,db)
